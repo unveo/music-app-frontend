122adba0ab88e609a8d0cf53a351980a412842af
ocenb <179301731+ocenb@users.noreply.github.com>
2025-02-05 19:09:58 +0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Providers from './providers';
 import { Toaster } from '@/components/ui/toaster';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<Providers>
 					{children}
+					<Analytics />
 					<Toaster />
 				</Providers>
 			</body>
